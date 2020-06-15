e1cd163b1cdb1108789dd1991b2401eb7e8aa2ba
glebkiselev <12bny23gleb@gmail.com>
2020-06-15 20:35:53 +0300

    black footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,14 +3,14 @@
 		
 		<div class="contacts-wrap">
 			
-			<sub class="overline">Контакты</sub>
+			<sub class="overline color-secondary-main">Контакты</sub>
 
 			<div>
-				<h5 style="margin-bottom: 0.25rem">
-					<a href="mailto:dver99design@gmail.com">dver99design@gmail.com</a>
+				<h5>
+					<a href="mailto:dver99design@gmail.com" class="color-secondary-main">dver99design@gmail.com</a>
 				</h5>
 
-				<h5>telegram: @daria_ver99</h5>
+				<h5 class="color-secondary-main">telegram: @daria_ver99</h5>
 			</div>
 			
 		</div>
@@ -21,15 +21,20 @@
 <style scoped>
 
 .footer-wrap {
+	background-color: var(--primary-dark);
 	margin-top: 6.5rem;
-	margin-bottom: 12.5rem; 
+	height: 20.5rem;
+}
+
+.color-secondary-main {
+	color: var(--secondary-main);
 }
 
 .contacts-wrap {
 	grid-column: center-col;
 	display: flex;
 	flex-direction: column;
-	justify-content: space-between;
+	justify-content: center;
 }
 
 </style>
